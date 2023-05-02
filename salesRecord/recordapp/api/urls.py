42a7f248d4c61bd2e3f53d817971ffd85aabc877
IsaacKusi <kusiisaacimpact@yahoo.com>
2023-05-02 20:30:37 +0000

    final commit

--- salesRecord/recordapp/api/urls.py
+++ salesRecord/recordapp/api/urls.py
@@ -1,17 +1,17 @@
 
 from django.urls import path
-from . import views
 
-from .views import MyTokenObtainPairView, saleItemSerializerView
+
+from .views import MyTokenObtainPairView, SaleItemSerializerView, SaleItemsDelete, GetSalesSerializer
 
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-
 urlpatterns = [
-    path('', views.getUser, name='getUser'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('sale', saleItemSerializerView.as_view(), name='sale_item'),
+    path('sale', SaleItemSerializerView.as_view(), name='sale_item'),
+    path('sale/<int:item_id>/<int:user_id>/', SaleItemsDelete.as_view(), name='delete_item'),
+    path('sale/<int:user_id>/', GetSalesSerializer.as_view(), name='get_item'),
 ]
